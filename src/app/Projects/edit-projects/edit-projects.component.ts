dbd72cf7f4ee0a86be66a2274689e9bb426a49ff
ameshmjayaweera <ameshmbjyw97@gmail.com>
2021-05-25 11:30:01 +0530

    update project edit form

--- src/app/Projects/edit-projects/edit-projects.component.ts
+++ src/app/Projects/edit-projects/edit-projects.component.ts
@@ -2,10 +2,11 @@ import { ProjectService } from 'src/app/Services/project.service';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/project';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import Swal from 'sweetalert2/dist/sweetalert2.all.js';
 import {Observable} from 'rxjs';
 import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
 
 
 @Component({
@@ -30,14 +31,14 @@ import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} 
 
   // default map values
   zoom: number = 8;
-  markers: any[] = [
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
+  // markers: any[] = [
+  //   {
+  //     lat: 51.723858,
+  //     lng: 7.895982,
+  //     label: 'C',
+  //     draggable: true
+  //   }
+  // ]
 
   numberRegEx = /[-.0-9]+/;
 
@@ -54,7 +55,8 @@ import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} 
   progressImage : number;
 
   constructor(private formBuilder: FormBuilder, private route : ActivatedRoute,
-              private service : ProjectService,private storage : AngularFireStorage) {
+              private service : ProjectService,private storage : AngularFireStorage,
+              private router : Router) {
 
   }
 
@@ -66,6 +68,8 @@ import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} 
         next : (value : Project) => {
           value.accessible = value.accessible == 1 ? "open" : "private";
           this.data = value;
+          this.lat = parseFloat(this.data.latitude);
+          this.longitude = parseFloat(this.data.longitude);
         },
         error : () => {
           Swal.fire({
@@ -103,7 +107,186 @@ import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} 
   get f() { return this.form.controls; }
 
   updateProject(){
+    this.submitted = true;
+    //stop here if form is invalid
+    // if (this.form.invalid) {
+    //   return;
+    // }
 
+    let formData : any = new FormData();
+    formData.append('id', this.data.id);
+    formData.append('version_id',this.data.version_id);
+    formData.append('admin_id',this.data.admin_id);
+    formData.append('project_title', this.form.get('project_title').value);
+    formData.append('project_title', this.form.get('project_title').value);
+    formData.append('author', this.form.get('author').value);
+    formData.append('organisation', this.form.get('organisation').value);
+    formData.append('abstract', this.form.get('abstract').value);
+    formData.append('category', this.form.get('category').value);
+    formData.append('energy_strategy', this.form.get('energy_strategy').value);
+    formData.append('bulding_scale', this.form.get('bulding_scale').value);
+    formData.append('climate_zone', this.form.get('climate_zone').value);
+    formData.append('material', this.form.get('material').value);
+    formData.append('parameters', this.form.get('parameters').value);
+    formData.append('type_of_doc', this.form.get('type_of_doc').value);
+    formData.append('mode_of_info', this.form.get('mode_of_info').value);
+    formData.append('topic', this.form.get('topic').value);
+    formData.append('world_region', this.form.get('world_region').value);
+    formData.append('longitude', this.form.get('longitude').value);
+    formData.append('latitude', this.form.get('latitude').value);
+    formData.append('accessible', 'open');
+
+    const id = Math.random().toString(36).substring(2);
+
+    // if both files changed
+    if(this.isChangeFile && this.isChangeImage) {
+      const projectFileName = `${this.project_file.name}_${id}_version_${1}`;
+      const projectImageName = `${this.img_file.name}_${id}_version_${1}`;
+
+      // upload file
+      this.refFile = this.storage.ref(`files/${projectFileName}`);
+      this.taskFile = this.refFile.put(this.project_file);
+      this.uploadProgressFile = this.taskFile.percentageChanges();
+      this.uploadProgressFile.subscribe(
+        {
+          next : res => this.progressFile = res,
+          error : err => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Project file upload failed!',
+            })
+            this.onReset();
+          },
+          complete : () => {
+            // upload image
+            this.refImage = this.storage.ref(`images/${projectImageName}`);
+            this.taskImage = this.refImage.put(this.img_file);
+            this.uploadProgressImage = this.taskImage.percentageChanges();
+            this.uploadProgressImage.subscribe(
+              {
+                next : res => this.progressImage = res,
+                error : err => {
+                  Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Project image upload failed!',
+                  })
+                  this.onReset();
+                },
+                complete : () => {
+                  this.taskFile.snapshotChanges().pipe(
+                    finalize(() => {
+                      this.refFile.getDownloadURL().subscribe(fileUrl => {
+                        formData.append('project_file',fileUrl);
+                        this.taskImage.snapshotChanges().pipe(
+                          finalize(() => {
+                            this.refImage.getDownloadURL().subscribe(imageUrl => {
+                              formData.append('img_file',imageUrl);
+                              this.callApi(formData);
+                            });
+                          })
+                        ).subscribe();
+                      });
+                    })
+                  ).subscribe();
+                }
+              }
+            );
+          }
+        }
+      );
+    } else if(this.isChangeFile && !this.isChangeImage) {
+
+      const projectFileName = `${this.project_file.name}_${id}_version_${1}`;
+
+      // upload file
+      this.refFile = this.storage.ref(`files/${projectFileName}`);
+      this.taskFile = this.refFile.put(this.project_file);
+      this.uploadProgressFile = this.taskFile.percentageChanges();
+      this.uploadProgressFile.subscribe(
+        {
+          next : res => this.progressFile = res,
+          error : err => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Project file upload failed!',
+            })
+            this.onReset();
+          },
+          complete : () => {
+            this.taskFile.snapshotChanges().pipe(
+              finalize(() => {
+                this.refFile.getDownloadURL().subscribe(fileUrl => {
+                  formData.append('project_file',fileUrl);
+                  this.callApi(formData);
+                });
+              })
+            ).subscribe();
+          }
+        }
+      );
+    } else if(!this.isChangeFile && this.isChangeImage) {
+      const projectImageName = `${this.img_file.name}_${id}_version_${1}`;
+
+      // upload image
+      this.refImage = this.storage.ref(`images/${projectImageName}`);
+      this.taskImage = this.refImage.put(this.img_file);
+      this.uploadProgressImage = this.taskImage.percentageChanges();
+      this.uploadProgressImage.subscribe(
+        {
+          next : res => this.progressImage = res,
+          error : err => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Project image upload failed!',
+            })
+            this.router.navigate(['/project-list']);
+          },
+          complete : () => {
+            this.taskImage.snapshotChanges().pipe(
+              finalize(() => {
+                this.refImage.getDownloadURL().subscribe(imageUrl => {
+                  formData.append('img_file',imageUrl);
+                  this.callApi(formData);
+                });
+              })
+            ).subscribe();
+          }
+        }
+      );
+
+    } else {
+      console.log(this.form);
+      this.callApi(formData);
+    }
+  }
+
+  callApi(formData) {
+    this.service.updateProject(formData)
+      .subscribe({
+        next : apiResponse => {
+        },
+        error : err => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something Went Wrong!',
+
+          })
+          this.router.navigate(['/project-list']);
+        },
+        complete : () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: 'Project Updated Successfully!',
+          });
+          this.router.navigate(['/project-list']);
+        }
+      });
   }
 
   onReset() {
@@ -125,11 +308,13 @@ import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} 
   projectFileChange(event) {
     let fileList: File = event.target.files;
     this.project_file=fileList[0];
+    this.isChangeFile = true;
   }
 
   imgFileChange(event) {
     let fileList: File = event.target.files;
     this.img_file=fileList[0];
+    this.isChangeImage = true;
   }
 
 }
--- src/app/Services/project.service.ts
+++ src/app/Services/project.service.ts
@@ -21,8 +21,8 @@ export class ProjectService {
     return this.httpClient.post('http://127.0.0.1:8000/api/uploadProject', project);
   }
 
-  updateProject(id, data){
-    return this.httpClient.put('http://127.0.0.1:8000/api/updateProject'+id,data);
+  updateProject(project){
+    return this.httpClient.post('http://127.0.0.1:8000/api/updateProject',project);
   }
 
   deleteProject(id){
